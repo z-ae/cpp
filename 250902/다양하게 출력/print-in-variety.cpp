bcb9499b14ede18f5ff05b8feb884edd73f2abb6
z-ae <jiea031225@gmail.com>
2025-09-02 22:48:40 +0900

    Title: 다양하게 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250902/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout << "Total days in Year\n365\nCircumference rate\n3.1415926535";
+    return 0;
+}
