451c316d76d29eae693e853a95105cd45b887804
z-ae <jiea031225@gmail.com>
2025-09-05 10:02:27 +0900

    Title: 변수 출력하기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250905/변수 출력하기 2/outputing-variables-2.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 3;
+    char b = 'C';
+    string c = "!.....!";
+
+    cout << b << c << a;
+
+    return 0;
+}
