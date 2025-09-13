40db91402592e957261d121802936044d578a2cb
z-ae <jiea031225@gmail.com>
2025-09-13 16:55:36 +0900

    Title: 정수 복사, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250913/정수 복사/copy-integer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 3;
+    int b = 4;
+
+    b = a;
+    cout << a << " " << b << '\n' << a*b;
+    return 0;
+}
