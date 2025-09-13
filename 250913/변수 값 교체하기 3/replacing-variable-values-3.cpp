1bb2ba4a7bded767828232f166b4e7c4bf60e096
z-ae <jiea031225@gmail.com>
2025-09-13 16:57:55 +0900

    Title: 변수 값 교체하기 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250913/변수 값 교체하기 3/replacing-variable-values-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 3;
+    int b = 5;
+    int tmp = 0;
+    tmp = a;
+    a = b;
+    b = tmp;
+    cout << a << '\n' << b;
+    return 0;
+}
