d2c46be84aa1c5d24fd52a1d66ac770a9d76fded
z-ae <jiea031225@gmail.com>
2025-09-15 21:19:08 +0900

    Title: 날짜 변경하여 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250915/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int y = 0, m = 0, d = 0;
+    char t = 0;
+
+    cin >> y >> t >> m >> t >> d;
+    cout << m << "-" << d << "-" << y;
+    return 0;
+}
