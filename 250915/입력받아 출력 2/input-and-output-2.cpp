022557dce2bfaf89cc57fbe6ec39d3e537177607
z-ae <jiea031225@gmail.com>
2025-09-15 21:17:31 +0900

    Title: 입력받아 출력 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250915/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int front = 0, back = 0;
+    cin >> front;
+    cin.get();
+    cin >> back;
+
+    cout << front << back;
+    
+    return 0;
+}
