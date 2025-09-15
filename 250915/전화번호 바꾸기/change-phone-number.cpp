a9f424cec89c434f4c7d328936531673895cd399
z-ae <jiea031225@gmail.com>
2025-09-15 21:23:30 +0900

    Title: 전화번호 바꾸기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250915/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 0, b = 0, c = 0;
+    char t = 0;
+    cin >> a >> t >> b >> t >> c;
+    cout << 0;
+    cout << a << t << c << t << b; 
+    return 0;
+}
