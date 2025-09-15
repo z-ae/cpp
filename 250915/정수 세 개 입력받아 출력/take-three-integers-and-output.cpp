5b8c828aac82dbb27dcc4eae161b4c01244367e5
z-ae <jiea031225@gmail.com>
2025-09-15 20:30:48 +0900

    Title: 정수 세 개 입력받아 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250915/정수 세 개 입력받아 출력/take-three-integers-and-output.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 0, b = 0, c = 0;
+    cin >> a >> b >> c;
+    cout << a << " " << b << " " << c;
+    return 0;
+}
