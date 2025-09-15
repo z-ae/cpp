4ba783469afdae899eee730566756d133669e748
z-ae <jiea031225@gmail.com>
2025-09-15 21:13:29 +0900

    Title: 1시간 뒤 시간 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250915/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int h = 0, m = 0;
+    cin >> h;
+    cin.get();
+    cin >> m;
+
+    cout << h + 1 << ":" << m;
+    
+    return 0;
+}
