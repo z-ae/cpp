6e3f020d14c77999dac73a1cc8fcca672d9b5238
z-ae <jiea031225@gmail.com>
2025-09-15 20:16:49 +0900

    Title: 실수 받아 그대로 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250915/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double N = 0;
+    cin >> N;
+
+    cout << fixed;
+    cout.precision(2);
+    cout << N;
+    return 0;
+}
