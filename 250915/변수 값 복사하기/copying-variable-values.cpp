be7ad02d5dd72189297ccfaa7ac3d78c490332ff
z-ae <jiea031225@gmail.com>
2025-09-15 20:09:54 +0900

    Title: 변수 값 복사하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250915/변수 값 복사하기/copying-variable-values.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 1, b = 2, c = 3;
+    a = c;
+    b = c;
+    cout << a << " " << b << " " << c;
+    
+    return 0;
+}
