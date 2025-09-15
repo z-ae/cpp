3a05cdabe34e95afc4e70bbfce4d26cea012628d
z-ae <jiea031225@gmail.com>
2025-09-15 20:20:12 +0900

    Title: 실수 입력받아 계산 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250915/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a = 0;
+    cin >> a;
+
+    cout << fixed;
+    cout.precision(2);
+
+    cout << a + 1.5;
+    return 0;
+}
