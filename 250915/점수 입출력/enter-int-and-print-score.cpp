b1cae53c7833dfa37707004e75bc39f4eaf20303
z-ae <jiea031225@gmail.com>
2025-09-15 20:13:37 +0900

    Title: 점수 입출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250915/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N = 0;
+    cin >> N;
+
+    cout << "Your score is " << N << " point.";
+    return 0;
+}
