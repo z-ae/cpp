0b6ac2dfdfa0826e78e085cf6bd19b86b5a6c28d
z-ae <jiea031225@gmail.com>
2025-09-22 00:25:32 +0900

    Title: 간단한 사칙연산, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250922/간단한 사칙연산/simple-arithmetic-operation.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 0, b = 0;
+    cin >> a >> b;
+
+    cout << a + b << "\n" << a - b << "\n" << a / b << "\n" << a % b;
+    return 0;
+}
