85eb9f63193d266ba89daebd7fc26807cb45339a
z-ae <jiea031225@gmail.com>
2025-09-25 12:43:20 +0900

    Title: 입력받아 연산하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250925/입력받아 연산하기/take-input-and-operate.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int  a  =0, b = 0;
+    cin >> a >> b;
+    a += 87;
+    b %= 10;
+    cout << a << "\n" << b;
+    return 0;
+}
