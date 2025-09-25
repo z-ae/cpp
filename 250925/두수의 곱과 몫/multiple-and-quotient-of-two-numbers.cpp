095b14409743962afe79cb9167e8ba8de54da6b5
z-ae <jiea031225@gmail.com>
2025-09-25 12:39:14 +0900

    Title: 두수의 곱과 몫, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250925/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 0, b = 0;
+    cin >> a >> b;
+    
+    cout << a << " * " << b << " = " << a * b << "\n";
+    cout << a << " / " << b << " = " << a / b << "\n";
+    return 0;
+}
