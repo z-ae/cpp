902e27882ddb7359876c213bb09e90576b94bcf0
z-ae <jiea031225@gmail.com>
2025-09-25 22:00:53 +0900

    Title: 음수 구별하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250925/음수 구별하기/separate-negative-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n = 0;
+    cin >> n;
+    cout << n << '\n';
+
+    if (n < 0){
+        cout << "minus";
+    }
+    return 0;
+}
