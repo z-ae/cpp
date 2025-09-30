09a0ca28f3a199d85b0498e9f5325af39ed95444
z-ae <jiea031225@gmail.com>
2025-09-30 20:49:48 +0900

    Title: 삼항연산자, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250930/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int score = 0;
+    cin >> score;
+
+    if (score == 100)
+        cout << "pass";
+    else
+        cout << "failure";
+    return 0;
+}
