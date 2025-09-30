e4e30cdb796170667f79159826208b10c4c44327
z-ae <jiea031225@gmail.com>
2025-09-30 21:01:45 +0900

    Title: 등급 매기기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250930/등급 매기기/ranking.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n = 0;
+    cin >> n;
+
+    if (n >= 90){
+        cout << "A";
+    }
+    else if (n >= 80){
+        cout << "B";
+    }
+    else if (n >= 70){
+        cout << "C";
+    }
+    else if (n >= 60){
+        cout << "D";
+    }
+    else
+        cout << "F";
+    return 0;
+}
