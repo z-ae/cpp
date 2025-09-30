892c7304268000305e9158c5bdf9cbeed84a5750
z-ae <jiea031225@gmail.com>
2025-09-30 21:07:39 +0900

    Title: 살 수 있는 물건 2, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250930/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n = 0;
+    cin >> n;
+
+    if (n < 500){
+        cout << "no";
+    }
+    else if (n < 1000){
+        cout << "pen";
+    }
+    else if (n < 3000) {
+        cout << "mask";
+    }
+    else{
+        cout << "book";
+    }
+    return 0;
+}
