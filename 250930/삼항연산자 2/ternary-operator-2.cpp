5e9dcbfaec0780479188e7f9c936ff3152bee4f4
z-ae <jiea031225@gmail.com>
2025-09-30 20:50:47 +0900

    Title: 삼항연산자 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250930/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 0;
+    cin >> a;
+
+    if (a == 1){
+        cout << "t";
+    }
+    else
+        cout << "f";
+    return 0;
+}
