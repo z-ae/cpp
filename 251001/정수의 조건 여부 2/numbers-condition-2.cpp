17b62959adef1ee5af01bb7cf9c879cbca462c86
z-ae <jiea031225@gmail.com>
2025-10-01 15:04:24 +0900

    Title: 정수의 조건 여부 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251001/정수의 조건 여부 2/numbers-condition-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 0;
+    cin >> a;
+
+    if (a == 5){
+        cout << 'A';
+    }
+    else if (a % 2 == 0){
+        cout << 'B';
+    }
+    return 0;
+}
