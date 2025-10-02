7c1bad460ef029cf476660e6d8e4adc6e4f193de
z-ae <jiea031225@gmail.com>
2025-10-02 15:19:02 +0900

    Title: 범위 안의 수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251002/범위 안의 수/number-in-range.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 0;
+    cin >> a;
+
+    if (a >= 10 && a <=20){
+        cout << "yes";
+    }
+    else cout << "no";
+    return 0;
+}
