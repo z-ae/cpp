0693373a5605ed495b3e7415707680d3b554ce58
z-ae <jiea031225@gmail.com>
2025-10-02 15:17:51 +0900

    Title: 점수 비교, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251002/점수 비교/score-comparison.cpp
+++ 251002/점수 비교/score-comparison.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     // Please write your code here.
     int a_math = 0, a_eng = 0, b_math = 0, b_eng = 0;
-    cin >> a >> b;
+    cin >> a_math >> a_eng >> b_math >> b_eng;
 
     if (a_math > b_math && a_eng > b_eng){
         cout << 1;
