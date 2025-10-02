286eedff44d7530a06c14077696df14c9286873a
z-ae <jiea031225@gmail.com>
2025-10-02 15:16:52 +0900

    Title: 점수 비교, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251002/점수 비교/score-comparison.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a_math = 0, a_eng = 0, b_math = 0, b_eng = 0;
+    cin >> a >> b;
+
+    if (a_math > b_math && a_eng > b_eng){
+        cout << 1;
+    }
+    else cout << 0;
+    return 0;
+}
