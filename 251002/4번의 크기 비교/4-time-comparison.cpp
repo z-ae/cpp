6f902c4b603b212153077c144f8e3623c65569a2
z-ae <jiea031225@gmail.com>
2025-10-02 15:14:42 +0900

    Title: 4번의 크기 비교, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251002/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 0, b = 0,  c = 0, d = 0, e = 0;
+    cin >> a >> b >> c >> d >> e;
+
+    cout << (a > b) << "\n" << (a > c) << "\n" 
+        << (a > d) << "\n" << (a > e);
+    return 0;
+}
