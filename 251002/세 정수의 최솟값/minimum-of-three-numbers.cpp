b58a4abb39286ab9e49a29d085083b60c8374548
z-ae <jiea031225@gmail.com>
2025-10-02 15:36:35 +0900

    Title: 세 정수의 최솟값, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 251002/세 정수의 최솟값/minimum-of-three-numbers.cpp
+++ 251002/세 정수의 최솟값/minimum-of-three-numbers.cpp
@@ -8,9 +8,9 @@ int main() {
     cin >> a >> b >> c;
 
     int min = 0;
-    if ((a > b && b > c) || (a < b && a > c))         min = c;  // abc, bac
-    else if ((a > b && b < c) || (a < c && a > b))    min = b;  // acb, cab
-    else                        min = a;
+    if ((a >= b && b >= c) || (a <= b && a >= c))         min = c;  // abc, bac
+    else if ((a >= b && b <= c) || (a <= c && a >= b))    min = b;  // acb, cab
+    else                                                min = a;
 
     cout << min;
     return 0;
