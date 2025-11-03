d55bf9b4932f96223f5a43e989b835806a36cbd4
z-ae <jiea031225@gmail.com>
2025-11-03 14:41:31 +0900

    Title: 좀 더 어려운 수학 점수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251103/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
+++ 251103/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -8,16 +8,10 @@ int main() {
 
     cin >> a_m >> a_e >> b_m >> b_e;
 
-    if (a_m > b_m){
+    if (a_m > b_m || ((a_m == b_m) && (a_e > b_e))){
         cout << "A";
     }
-    else if (a_m < b_m){
-        cout << "B";
-    }
-    else if (a_e > b_e){
-        cout << "A";
-    }
-    else{
+    else if (a_m <= b_m || ((a_m == b_m) && (a_e < b_e))){
         cout << "B";
     }
     
