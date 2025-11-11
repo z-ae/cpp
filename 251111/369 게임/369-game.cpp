a8e46d26a424effaa25ccbf561b7fd8f9c5e3121
z-ae <jiea031225@gmail.com>
2025-11-11 20:47:57 +0900

    Title: 369 게임, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251111/369 게임/369-game.cpp
+++ 251111/369 게임/369-game.cpp
@@ -7,14 +7,25 @@ int main() {
     int num = 1;
 
     cin >> n;
-    
+
+/*  3 6 9       3으로 나눠떨어짐
+    13 16 19    10의 자릿수 && num을 10으로 나눈 나머지가 3으로 나눠떨어짐
+    30 31 ...   10의 자리가 3으로 나눠떨어짐 */
+
     while (n >= num){
-        if (num == 3 || num == 6 || num == 9){
+
+        if (num % 3 == 0)
             cout << 0 << " ";
+        else cout << num << " ";
+        
+        if (num / 10 > 0){
+            if      ((num % 10 != 0) && ((num % 10) % 3 == 0))   cout << 0<< " ";
+            else if ((num / 10) % 3 == 0)   cout << 0 << " ";
+            else                            cout << num << " ";
+
         }
-        else{
-            cout << num << " ";
-        }
+
+
         num++;
     }
 
