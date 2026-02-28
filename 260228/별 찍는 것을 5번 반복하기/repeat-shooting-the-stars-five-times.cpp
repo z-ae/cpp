4e28112e338d88ebb2d5d55c1eaf9929366cc1bb
z-ae <jiea031225@gmail.com>
2026-02-28 12:24:38 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260228/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void Print10Star() {
+    for (int i = 0; i < 10; i++){
+        printf("*");
+    }
+    printf("\n");
+}
+
+int main() {
+    // Please write your code here.
+
+    for (int i = 0; i <5; i++){
+        Print10Star();
+    }
+    return 0;
+}
+
+
